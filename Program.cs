4652576da1d66c6ee61c77f81466d2d5e1e85028
Edistein <365802825@qq.com>
2024-05-31 02:13:15 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+
+const string OutputDir = "migrated";
+string[] files = Directory.GetFiles(".");
+Directory.CreateDirectory(OutputDir);
+
+JsonSerializerSettings settings = new()
+{
+    TypeNameHandling = TypeNameHandling.All
+};
+JsonSerializerOptions options = new()
+{
+    WriteIndented = true,
+};
+
+foreach (var file in files)
+{
+    if (file.EndsWith(".json") )
+    {
+        await MigrateFile(file);
+        Console.WriteLine($"{file} migrated.");
+    }
+}
+
+Console.WriteLine("\nDone.");
+Console.ReadKey(true);
+
+async Task MigrateFile(string file)
+{
+    string content = await File.ReadAllTextAsync(file);
+    Dictionary<string, object?>? dict = JsonConvert.DeserializeObject<Dictionary<string, object?>>(content, settings)
+        ?? throw new Exception($"Failed to convert file [{file}]");
+    content = System.Text.Json.JsonSerializer.Serialize(dict, options);
+    await File.WriteAllTextAsync(Path.Combine(OutputDir, file), content);
+}
